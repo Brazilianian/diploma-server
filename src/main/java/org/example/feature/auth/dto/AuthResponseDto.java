fa247fceee3ac4eee00fbaeffa0c77195666d41b
vdolhushyn <vetajika@gmail.com>
2024-03-20 10:21:52 +0200

    fix: auth response dto naming

--- src/main/java/org/example/feature/auth/dto/AuthResponseDto.java
+++ src/main/java/org/example/feature/auth/dto/AuthResponseDto.java
@@ -3,7 +3,7 @@ package org.example.feature.auth.dto;
 import org.example.feature.user.dto.UserDto;
 
 public record AuthResponseDto(
-        UserDto userDto,
-        TokensDto tokensDto
+        UserDto user,
+        TokensDto tokens
 ) {
 }
